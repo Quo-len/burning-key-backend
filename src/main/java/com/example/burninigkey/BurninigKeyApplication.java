c3f89845f6f2f66e62aed85e1e5139b0e010881d
parfesa.a15@gmail.com <parfesa.a15@gmail.com>
2024-05-19 21:35:26 +0300

    first commit

---
+++ src/main/java/com/example/burninigkey/BurninigKeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.burninigkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurninigKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurninigKeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/burninigkey/BurninigKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.burninigkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurninigKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
